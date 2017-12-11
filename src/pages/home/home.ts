9a6ad6cd72ff9276a19da3f9884a00c37d728117
youssef <youssef0918548@gmail.com>
2017-12-11 16:38:49 +0300

    Add All Files

---
+++ src/pages/home/home.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+// import { HomePage} from '../home';
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+
+export class HomePage {
+  slides = [
+    {
+      title: "ATR Contracting Investment & Services Co.Ltd",
+      description: " ",
+      image: "../assets/Capture.PNG",
+    },
+    
+    {
+      title: "ATR Contracting Investment & Services Co.Ltd",
+      description: " ",
+      image: "../assets/clid2.PNG",
+    },
+    {
+      title: "ATR Contracting Investment & Services Co.Ltd",
+      description: " ",
+      image: "../assets/clid3.PNG",
+    },
+    {
+      title: "ATR Contracting Investment & Services Co.Ltd",
+      description: " ",
+      image: "../assets/clid4.PNG",
+    },
+    {
+      title: "ATR Contracting Investment & Services Co.Ltd",
+      description: " ",
+      image: "../assets/clid5.PNG",
+    },
+    {
+      title: "ATR Contracting Investment & Services Co.Ltd",
+      description: " ",
+      image: "../assets/clid6.PNG",
+    },
+    {
+      title: "ATR Contracting Investment & Services Co.Ltd",
+      description: " ",
+      image: "../assets/clid7.PNG",
+    }
+    
+  ];
+  
+  
+}
